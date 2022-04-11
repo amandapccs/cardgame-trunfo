3360981fff80a314d2c27b29e6a4341215701900
Amanda Soares <amandchen@hotmail.com>
2022-04-11 15:40:34 -0300

    Adiciona requisito 3

---
+++ src/components/Card.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const {
+      cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
+    return (
+      <>
+        <p data-testid="name-card">{ cardName }</p>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <p data-testid="description-card">{ cardDescription }</p>
+        <p data-testid="attr1-card">{ cardAttr1 }</p>
+        <p data-testid="attr2-card">{ cardAttr2 }</p>
+        <p data-testid="attr3-card">{ cardAttr3 }</p>
+        <p data-testid="rare-card">{ cardRare }</p>
+        { cardTrunfo && <p data-testid="trunfo-card">Super Trunfo</p> }
+      </>
+    );
+  }
+}
+
+export default Card;
